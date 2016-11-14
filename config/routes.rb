41d9598629a52664deab784b2d9d01436f6e9a92
nat14 <naryucha14@gmail.com>
2016-11-13 21:54:26 -0800

    Make text shout polumorphism

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#show', via: :get
-  resources :shouts, only: [:create, :show]
+  resources :shouts, only: [:show]
+  resources :text_shouts, only: [:create]
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
