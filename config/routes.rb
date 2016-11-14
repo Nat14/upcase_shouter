f151af03e48e4757bca8e84198765a96f0719cc8
nat14 <naryucha14@gmail.com>
2016-11-13 19:29:41 -0800

    Add shout show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#show', via: :get
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
