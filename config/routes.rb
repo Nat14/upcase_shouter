11ed3a6f6dc2a40763af04ffe3a432fe9f467d52
nat14 <naryucha14@gmail.com>
2016-11-14 08:11:06 -0800

    Add photo shout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#show', via: :get
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
+  resources :photo_shouts, only: [:create]
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
--- db/migrate/20161114160345_create_photo_shouts.rb
+++ db/migrate/20161114160345_create_photo_shouts.rb
@@ -2,8 +2,6 @@ class CreatePhotoShouts < ActiveRecord::Migration[5.0]
   def change
     create_table :photo_shouts do |t|
       t.attachment :image
-
-      t.timestamps
     end
   end
 end
