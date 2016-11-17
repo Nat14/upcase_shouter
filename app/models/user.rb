05d5885beb0dff167063e2693c9fa0a06a026fe4
nat14 <naryucha14@gmail.com>
2016-11-16 20:18:36 -0800

    Add model following relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :shouts
+
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
 end
